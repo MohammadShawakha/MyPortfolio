c8ee10c9054b2848cd9c0078e92bc7d7b2e3a98d
MohammadShawakha <mohammad.shawakha@students.alquds.edu>
2024-07-24 01:04:36 +0300

    make gh page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./nav-bar/nav-bar";
 import ListPage from "./list-page/list-page.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NavBar />
