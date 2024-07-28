e5e0a6023e850cb21c4f4d588e838d0220253037
MohammadShawakha <mohammad.shawakha@students.alquds.edu>
2024-07-28 16:28:32 +0300

    add qr-code app

--- src/appsData/appsData.js
+++ src/appsData/appsData.js
@@ -8,33 +8,12 @@ const appData = [
         appUrl: "https://mohammadshawakha.github.io/React-Js-Full-toDoApp/",
       },{
         key:"2",
-        appName: "222",
-        appImage:"to-do-app.png",
+        appName: "QR Code Generator App",
+        appImage:"qr-code-generator-app.png",
         appDescription:
-          "222",
-        appUrl: "https://mohammadshawakha.github.io/React-Js-Full-toDoApp/",
-      },{
-        key:"3",
-        appName: "333",
-        appImage:"to-do-app.png",
-        appDescription:
-          "3333",
-        appUrl: "https://mohammadshawakha.github.io/React-Js-Full-toDoApp/",
-      },{
-        key:"4",
-        appName: "444",
-        appImage:"to-do-app.png",
-        appDescription:
-          "44444",
-        appUrl: "https://mohammadshawakha.github.io/React-Js-Full-toDoApp/",
-      },{
-        key:"5",
-        appName: "555",
-        appImage:"to-do-app.png",
-        appDescription:
-          "5555",
-        appUrl: "https://mohammadshawakha.github.io/React-Js-Full-toDoApp/",
-      },
+          "This is a Qr-code generator app build using React js.",
+        appUrl: "https://mohammadshawakha.github.io/QR-Code-App/",
+      }
   ];
   
   const getAppData = () => {
